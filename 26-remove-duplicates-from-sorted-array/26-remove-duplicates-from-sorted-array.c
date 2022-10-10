a0d43db1191a1dc5261382a2cea11a5c40ee4ad4
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2022-10-09 20:05:37 -0600

    Time: 36 ms (12.64%), Space: 7.5 MB (77.20%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.c
@@ -0,0 +1,23 @@
+
+
+int removeDuplicates(int* nums, int numsSize){
+    
+    int j=0;
+    
+    if (numsSize==0 || numsSize==1)
+    {
+        return numsSize;
+    }
+    
+    for(int i = 0; i<numsSize-1; i++)
+    {
+        if (nums[i]!=nums[i+1])
+        {
+            nums[j++]=nums[i];
+        }     
+    }
+    
+    nums[j++] = nums[numsSize-1];  
+    
+    return j;
+}
