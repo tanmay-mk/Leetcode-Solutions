3651767217fc2b26a3beeac599a5e16e7c962545
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2022-10-10 00:03:18 -0600

    Time: 3 ms (41.79%), Space: 5.4 MB (98.51%) - LeetHub

---
+++ 868-binary-gap/868-binary-gap.c
@@ -0,0 +1,49 @@
+
+
+int binaryGap(int n){
+    
+    int current_distance = 0, max_distance = 0; 
+    bool flag = false;
+    
+    /*get the index of first 1 in the number*/
+    int firstIdx = 0;
+    for (int i = 0; i < 31; i++)
+    {
+        if ((n & (1 << i)) != 0)
+        {
+            firstIdx = i;
+            break;
+        }
+    }
+    
+    for (int i = firstIdx; i<31; i++)
+    {
+        if ((n & (1 << i)) == 0)
+        {
+            current_distance++;
+        }
+        else
+        {
+            
+            if(current_distance == 0)
+            {
+                flag = true; 
+            }
+            else
+            {
+                flag = false;
+            }
+            
+            if(current_distance > max_distance || flag == true)
+            {
+                flag= false;
+                max_distance = current_distance; 
+            }
+            current_distance = 1;
+        }
+        
+
+    }
+    
+    return max_distance;
+}
