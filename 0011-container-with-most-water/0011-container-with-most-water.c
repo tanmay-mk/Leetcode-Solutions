43e09070766a5002f1c10a8f2cb380ca6ce0aa04
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2022-10-12 13:47:21 -0600

    Time: 173 ms (53.42%), Space: 11.7 MB (58.79%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.c
@@ -0,0 +1,35 @@
+
+
+int maxArea(int* height, int heightSize){
+    
+    int max_area = 0;
+    int length = 0, breadth = 0, area = 0; 
+    
+    int maxLength = 0, maxBreadth = 0;
+    
+    int i = 0, j = heightSize - 1;
+    
+    while(i <= j)
+    {
+        length = j-i;
+        breadth = (height[i]<height[j])?height[i]:height[j];
+        
+        area = length*breadth;
+        
+        if (max_area < area)
+        {
+            max_area = area;
+        }
+        
+        if (height[i]<height[j])
+        {
+            i++;
+        }
+        else
+        {
+            j--;
+        }
+    }
+    
+    return max_area;
+}
