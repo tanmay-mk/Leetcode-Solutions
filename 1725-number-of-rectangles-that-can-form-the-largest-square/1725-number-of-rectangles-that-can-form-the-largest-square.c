8ae67c8d6439becf38f4adb2ba95e32aa21d8e3c
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2022-10-17 11:39:40 -0600

    Time: 80 ms (35.00%), Space: 8.3 MB (15.00%) - LeetHub

---
+++ 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.c
@@ -0,0 +1,30 @@
+
+
+int countGoodRectangles(int** rectangles, int rectanglesSize, int* rectanglesColSize){
+
+        int count = 0;
+        int max = 0;
+
+        int *minSides = (int *)malloc(sizeof(int)*rectanglesSize);
+
+        /*get smallest side of each rectangle*/
+        for(int i=0; i<rectanglesSize; i++)
+        {
+            int min = 0; 
+            min = (rectangles[i][0] < rectangles[i][1])? rectangles[i][0] : rectangles[i][1]; 
+            minSides[i] = min;
+            if(max < min)
+            {
+                max = min;
+            }
+        }
+
+        for(int i = 0; i<rectanglesSize; i++)
+        {
+            if(minSides[i] == max)
+            {
+                count++;
+            }
+        }
+        return count; 
+}
