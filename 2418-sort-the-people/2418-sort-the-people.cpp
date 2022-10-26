34eba0318576f491a68778b3d8df9771a19a6301
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2022-10-25 18:24:16 -0600

    Time: 118 ms (12.62%), Space: 20.2 MB (92.07%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        
+        vector<int> sortedHeights (heights);
+        
+        sort(sortedHeights.begin(), sortedHeights.end(), greater<int>());
+        
+        vector<string> sortedNames; 
+        
+        int namesSize = names.size();
+        
+        for (int i = 0; i < namesSize; i++)
+        {
+            int nameIdx = 0;
+            int height = sortedHeights[i];
+            for (int j = 0; j < namesSize; j++)
+            {
+                if (heights[j] == height)
+                {
+                    nameIdx = j;
+                    break;
+                }
+            }
+            sortedNames.push_back(names[nameIdx]);
+        }
+        return sortedNames;
+    }
+};
