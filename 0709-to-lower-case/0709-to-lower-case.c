69062d31405b6a99bb8e975d1ce8886875902782
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2022-10-29 21:41:15 -0600

    Time: 6 ms (12.14%), Space: 5.9 MB (9.25%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.c
@@ -0,0 +1,20 @@
+
+
+char * toLowerCase(char * s){
+
+    int sSize = strlen(s);
+    
+    for (int i=0; i<sSize; i++)
+    {
+        if (!(s[i] >= 'a' && s[i] <= 'z'))
+        {
+            if (s[i] >= 'A' && s[i] <= 'Z')
+            {
+                s[i] += 32;
+            }           
+        }
+    }
+    
+    
+    return s; 
+}
