e7fe7cd44376d095171a35e6ef59fc48270410f2
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-02-03 10:31:29 -0700

    Time: 0 ms (100.00%), Space: 5.4 MB (75.00%) - LeetHub

---
+++ 1118-number-of-days-in-a-month/1118-number-of-days-in-a-month.c
@@ -0,0 +1,14 @@
+int numberOfDays(int year, int month){
+
+    int days[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    
+    if (
+        (month == 2) &&
+        ((year % 4) == 0) &&
+        (((year % 400) == 0) || ((year % 100) != 0))
+        )
+    {
+        return 29; 
+    }
+    return days[month];
+}
