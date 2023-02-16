60c91eb7521ad2d207a122ed6b419d039fcd6ed2
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-02-16 10:33:05 -0700

    Time: 58 ms (18.64%), Space: 104 MB (8.47%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.c
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.c
@@ -15,15 +15,19 @@ int numDigits(int num)
  */
 int* separateDigits(int* nums, int numsSize, int* returnSize){
     
-    //we will go in reverse direction, so we don't have to constantly reverse the final array
+    //when we separate digits from a number, the digits come off in reverse order
+    //in that way, we will have to reverse the array every time we separate digits from the number
+    
+    //instead, we will traverse the array in reverse direction
+    //separating digits in reverse direction, so we don't have to constantly reverse the array
+    //after processing each number
     //we will reverse the entire return array at once at the end
     
     int retSize = numDigits(nums[numsSize-1]); //find number of digits for last number in array
     int *arr = (int *)malloc(sizeof(int)*retSize);  //allocate memory
-    
     int idx = 0; //global idx to fill in return array
     
-    
+    //fill in digits of last number
     while(nums[numsSize-1]>0)
     {
         int rem = nums[numsSize-1]%10;
@@ -33,8 +37,11 @@ int* separateDigits(int* nums, int numsSize, int* returnSize){
     
     for(int i=numsSize-2; i>=0; i--)
     {
+        //compute new size of returned array
         retSize = retSize + numDigits(nums[i]); 
-        arr = (int *) realloc(arr, retSize*sizeof(int));
+        //reallocate memory
+        arr = (int *)realloc(arr, retSize*sizeof(int));
+        //separate & store digits
         while(nums[i]>0)
         {
             int rem = nums[i]%10;
@@ -42,10 +49,10 @@ int* separateDigits(int* nums, int numsSize, int* returnSize){
             nums[i] /= 10; 
         }
     }
-
+    //update return size
     *returnSize = retSize; 
     
-    //reverse
+    //reverse the array
     for(int i=0; i<retSize/2; i++)
     {
         arr[i] ^= arr[retSize - i - 1];
