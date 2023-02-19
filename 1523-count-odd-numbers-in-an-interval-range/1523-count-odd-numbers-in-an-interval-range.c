e8e42f62a44e95c010bce0ed1071464db580d64b
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-02-19 14:26:52 -0700

    Time: 3 ms (50.49%), Space: 5.4 MB (98.15%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.c
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.c
@@ -4,24 +4,21 @@ int countOdds(int low, int high){
 
     int count=0;
     
-    bool low_odd = false, high_odd = false;
-    
     if((low & 1) == 1)
     {
-        low_odd = true;
         count++;
     }
     
     if((high & 1) == 1)
     {
-        high_odd = true;
         count++;
     }
      
     
-    if ((low_odd == true) && (high_odd == true))
+    if (((low & 1) == 1) && ((high & 1) == 1))
     {
         return (count + (((high - low) >> 1) - 1));
     }
+    
     return (count + ((high - low) >> 1));
 }
