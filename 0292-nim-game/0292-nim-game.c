5dfcf1eacecbf23b106e21b47983231f8bf9759c
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-02-20 16:36:55 -0700

    Time: 3 ms (45.71%), Space: 5.5 MB (62.86%) - LeetHub

--- 0292-nim-game/0292-nim-game.c
+++ 0292-nim-game/0292-nim-game.c
@@ -1,5 +1,12 @@
 bool canWinNim(int n){
 
+    /*
+    Let us think of the small cases. It is clear that if there are only one, two, or three stones in the pile, and it is your turn, you can win the game by taking all of them. Like the problem description says, if there are exactly four stones in the pile, you will lose. Because no matter how many you take, you will leave some stones behind for your opponent to take and win the game. So in order to win, you have to ensure that you never reach the situation where there are exactly four stones on the pile on your turn.
+
+Similarly, if there are five, six, or seven stones you can win by taking just enough to leave four stones for your opponent so that they lose. But if there are eight stones on the pile, you will inevitably lose, because regardless whether you pick one, two or three stones from the pile, your opponent can pick three, two or one stone to ensure that, again, four stones will be left to you on your turn.
+
+This will repeat for all multiples of 4, such as 8, 12, 16 and so on
+*/
+
     return ((n % 4) != 0);
-    
 }
