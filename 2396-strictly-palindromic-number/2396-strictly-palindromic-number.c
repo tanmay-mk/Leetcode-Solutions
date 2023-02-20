fc89091981e601de3d500d200c314f9b4617b8b6
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-02-20 15:45:00 -0700

    Time: 0 ms (100.00%), Space: 5.5 MB (49.26%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.c
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.c
@@ -1,9 +1,49 @@
+bool isPalindrome(char *str)
+{
+    int len = strlen(str);
+    
+    for(int i=0; i<len/2; i++)
+    {
+        if (str[i] != str[len - i - 1])
+        {
+            return false;
+        }
+    }
+    return true; 
+}
+
+char *calculate_base(int n, char* str, int base)
+{
+    int idx = 0;
+    while(n > 0)
+    {
+        int rem = n % base; 
+        str[idx++] = '0' + rem;
+        n /= base; 
+    }
+    
+    for (int i=0; i<idx/2; i++)
+    {
+        str[i] ^= str[idx - i - 1];
+        str[idx - i - 1] ^= str[i];
+        str[i] ^= str[idx - i - 1];
+    }
+    return str; 
+}
+
 bool isStrictlyPalindromic(int n){
+
+    char str[100];
+    memset(str, 0, (sizeof(char)*100));
     
-//      The given condition is very hard to achieve, upon checking it for some
-//      small integers we didnt get palindrome in every base. So for more larger numbers it will be impossible to get             palindrome in every base.
-//      So, simply return false in every case! :)
-    
-    
-    return false; 
+    for(int i = 2; i <= (n-2); i++)
+    {
+        calculate_base(n, str, i);
+        if (isPalindrome(str) == false)
+        {
+            return false;
+        }
+        memset(str, 0, (sizeof(char)*100));
+    }
+    return true; 
 }
