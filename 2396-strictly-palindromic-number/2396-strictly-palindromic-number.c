0493ab35fdc4890701513d31889cf318183368fa
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-02-20 15:36:11 -0700

    Time: 0 ms (100.00%), Space: 5.7 MB (25.00%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.c
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.c
@@ -1,3 +1,9 @@
 bool isStrictlyPalindromic(int n){
+    
+//      The given condition is very hard to achieve, upon checking it for some
+//      small integers we didnt get palindrome in every base. So for more larger numbers it will be impossible to get             palindrome in every base.
+//      So, simply return false in every case! :)
+    
+    
     return false; 
 }
