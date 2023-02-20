bf4eea41a8a0ef441aeece5b5fdf3f191d7bc056
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-02-20 16:17:53 -0700

    Time: 7 ms (41.42%), Space: 6.2 MB (71.81%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.c
@@ -0,0 +1,23 @@
+void swap(int *a, int *b)
+{
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
+}
+
+void rotate(int** matrix, int matrixSize, int* matrixColSize){
+
+    int n = matrixSize; 
+    for (int i=0; i<(matrixSize + 1)/2; i++)
+    {
+        for (int j=0; j<matrixSize/2; j++)
+        {
+            int temp = matrix[n - 1 - j][i];
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+            matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+            matrix[j][n - 1 - i] = matrix[i][j];                
+            matrix[i][j] = temp;
+        }
+    }
+    
+}
