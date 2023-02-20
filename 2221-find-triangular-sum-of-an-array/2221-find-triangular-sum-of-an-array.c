c076b42d119c674e6bd2483ee9938755c1957d73
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-02-20 15:54:42 -0700

    Time: 121 ms (56.10%), Space: 6.6 MB (29.27%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.c
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.c
@@ -4,11 +4,7 @@ int triangularSum(int* nums, int numsSize){
     {
         for(int i=0; i<numsSize-1; i++)
         {
-            nums[i] = nums[i] + nums[i+1];
-            if (nums[i] > 9)
-            {
-                nums[i] = nums[i] % 10;
-            }
+            nums[i] = ((nums[i] + nums[i+1]) % 10);
         }
         numsSize--;
     }
