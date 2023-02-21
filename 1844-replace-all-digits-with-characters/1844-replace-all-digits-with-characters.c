0bb6dc928296f9dd2255a0420adc42a9cbb8af6e
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-02-21 12:53:21 -0700

    Time: 4 ms (38.18%), Space: 5.7 MB (61.82%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.c
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.c
@@ -3,9 +3,8 @@ char * replaceDigits(char * s){
     int len = strlen(s);
     
     for(int i=1; i<len; i+=2)
-    {
-        int shift = s[i] - '0'; 
-        s[i] = s[i-1] + shift; 
+    { 
+        s[i] = s[i-1] + (s[i] - '0'); 
     }
     return s;
 }
