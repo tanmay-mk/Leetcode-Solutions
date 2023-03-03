31f84650ff7beadb9e90ec885af1746c72cb31b0
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-03-03 00:39:02 -0700

    Time: 4 ms (45.28%), Space: 5.4 MB (81.13%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.c
@@ -0,0 +1,21 @@
+#define MIN(a, b) ((a<b)?a:b)
+
+char repeatedCharacter(char * s){
+
+    int sLen = strlen(s);
+    int minIdx = sLen; 
+    
+    for(int i=0; i<sLen; i++)
+    {
+        for(int j=i+1; j<sLen; j++)
+        {
+            if(s[i] == s[j])
+            {
+                minIdx = MIN(minIdx, j);
+            }
+        }
+    }
+    
+    //return
+    return s[minIdx];
+}
