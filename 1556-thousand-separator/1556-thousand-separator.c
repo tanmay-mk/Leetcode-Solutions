8842e3f32680928d1554dc222339b9638694a7bb
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-03-03 15:43:38 -0700

    Time: 0 ms (100.00%), Space: 5.7 MB (16.67%) - LeetHub

---
+++ 1556-thousand-separator/1556-thousand-separator.c
@@ -0,0 +1,53 @@
+char * thousandSeparator(int n){
+
+    if (n == 0)
+    {
+        return "0";
+    }
+    
+    int len = 0; 
+    
+    char *str = (char *)malloc(sizeof(char)*len);
+    memset(str, 0, (sizeof(char)*len));
+    int idx = 0; 
+    int count = 0; 
+    bool flag = false; 
+    
+    while(n > 0)
+    {
+        if(count == 3)
+        {
+            len++;
+            count = 0; 
+            flag = true; 
+        }
+        len++; 
+        int rem = n % 10; 
+        str = (char *)realloc(str, sizeof(char)*len); 
+        if (flag)
+        {
+            flag = false;
+            str[idx++] = '.'; 
+            str[idx++] = rem + '0'; 
+        }
+        else
+        {
+            str[idx++] = rem + '0'; 
+        }
+        n /= 10;
+        count++; 
+    }
+    len++; 
+    str = (char *)realloc(str, sizeof(char)*len);
+    str[idx] = '\0';
+    
+    for(int i=0; i<idx/2; i++)
+    {
+        str[i] ^= str[idx-i-1];
+        str[idx-i-1] ^= str[i];
+        str[i] ^= str[idx-i-1];
+    }
+    
+    
+    return str;    
+}
