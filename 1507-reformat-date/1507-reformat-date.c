62fd8aa6c6e21520c9cb6b8a7c9216930e04a9ec
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-03-04 01:15:43 -0700

    Time: 3 ms (46.15%), Space: 5.6 MB (38.46%) - LeetHub

---
+++ 1507-reformat-date/1507-reformat-date.c
@@ -0,0 +1,120 @@
+char * reformatDate(char * date){
+    
+    char *nDate = (char *)malloc(sizeof(char)*11); 
+    
+    int dLen = strlen(date);
+    
+    nDate[0] = date[dLen-4]; 
+    nDate[1] = date[dLen-3]; 
+    nDate[2] = date[dLen-2]; 
+    nDate[3] = date[dLen-1];
+    nDate[4] = '-';
+    
+    //get and process month
+    int idx=0;
+    while (!(date[idx] >= 'A' && date[idx] <= 'Z'))
+    {
+        idx++; 
+    }
+    
+    switch(date[idx])
+    {
+        //check for Jan, Jun or Jul
+        case 'J':
+            if(date[idx+1] == 'a')
+            {
+                nDate[5] = '0';
+                nDate[6] = '1'; 
+            }
+            else if(date[idx+1] == 'u')
+            {
+                if(date[idx+2] == 'n')
+                {
+                    nDate[5] = '0';
+                    nDate[6] = '6';
+                }
+                else
+                {
+                    nDate[5] = '0';
+                    nDate[6] = '7';
+                }
+            }
+        break; 
+        
+        case 'F': // February 
+            nDate[5] = '0';
+            nDate[6] = '2';  
+        break; 
+            
+        case 'M': //March or May
+            if(date[idx+1] == 'a' && date[idx+2] == 'r')
+            {
+                nDate[5] = '0';
+                nDate[6] = '3'; 
+            }  
+            else
+            {
+                nDate[5] = '0';
+                nDate[6] = '5';                 
+            }
+        break;     
+        
+        case 'A': //Apr or Aug
+            if(date[idx+1] == 'p')
+            {
+                nDate[5] = '0';
+                nDate[6] = '4'; 
+            }  
+            else
+            {
+                nDate[5] = '0';
+                nDate[6] = '8';                 
+            }
+        break;  
+            
+        case 'S': // Sept 
+            nDate[5] = '0';
+            nDate[6] = '9';  
+        break;
+            
+        case 'O': // Oct 
+            nDate[5] = '1';
+            nDate[6] = '0';  
+        break;
+            
+        case 'N': // Nov 
+            nDate[5] = '1';
+            nDate[6] = '1';  
+        break;
+            
+        case 'D': // Dec 
+            nDate[5] = '1';
+            nDate[6] = '2';  
+        break;
+        
+        default: 
+        break;
+    }
+    nDate[7] = '-'; 
+    
+    //get & process date
+    int dt = 0; 
+    for(int i=0; i<4; i++)
+    {
+        if(date[i] >= '0' && date[i] <= '9')
+        {
+            dt = (dt*10) + (date[i] - '0');
+        }
+        else
+        {
+            break; 
+        }
+    }
+    
+    nDate[9] = (dt % 10) + '0';
+    dt /= 10; 
+    nDate[8] = (dt % 10) + '0';
+    nDate[10] = '\0';
+    
+    return nDate; 
+}
