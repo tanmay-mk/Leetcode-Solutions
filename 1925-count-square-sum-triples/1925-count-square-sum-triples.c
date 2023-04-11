6c1189044b37b3a0d57beb561b4472feafc40506
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-04-11 10:38:26 -0600

    Time: 445 ms (10.20%), Space: 5.4 MB (63.27%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.c
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.c
@@ -3,11 +3,14 @@ int countTriples(int n){
     
     for(int c=1; c<=n; c++)
     {
+        int csqr = (c*c); 
         for(int a=1; a<=n; a++)
         {
+            int asqr = (a*a); 
             for(int b=1; b<=n; b++)
             {
-                if (((a*a)+(b*b)) == (c*c))
+                int bsqr = (b*b); 
+                if((asqr+bsqr)==csqr)
                 {
                     count++; 
                 }
