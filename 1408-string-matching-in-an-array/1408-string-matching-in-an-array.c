1377bfca62e6e9097d43ee6b0ea280f0a2eb1a5e
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-04-17 15:56:46 -0600

    Time: 10 ms (15.79%), Space: 6.3 MB (31.58%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.c
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.c
@@ -9,34 +9,41 @@ char ** stringMatching(char ** words, int wordsSize, int* returnSize){
     
     for(int i=0; i<wordsSize; i++)
     {
-        int thislen = strlen(words[i]);
+        int thislen = strlen(words[i]); //get length of word to be compared
         for(int j=0; j<wordsSize; j++)
         {
-            if(i != j)
-            {
-                int len = strlen(words[j]); 
-                if(thislen <= len)
+            if(i != j)      //do not compare same word to each other
+            {   
+                int len = strlen(words[j]); //get length of word which we want to compare with
+                if(thislen <= len)          //only compare if length of words[i] < words[j]
                 {
-                    for(int k=0; k<=(len-thislen); k++)
+                    for(int k=0; k<=(len-thislen); k++) //start comparing
                     {
-                        bool flag = false; 
-                        if(memcmp(&words[j][k], words[i], thislen) == 0)
+                        bool flag = false; //flag to see if sub-array already exists in array to be returned
+                        if(memcmp(&words[j][k], words[i], thislen) == 0)    //check for substring
                         {
+                            //substring found
                             for(int l=0; l<idx; l++)
                             {
+                                //see if substring already added
                                 if(strcmp(words[i], arr[l])==0)
                                 {
+                                    //string already added, set flag
                                     flag = true; 
                                     break; 
                                 }
                             }
-                            if(flag == false)
+                            if(flag == false)   //unique substring found
                             {
-                                numWords++; 
+                                numWords++;     //increment number of words
+                                //allocate memory
                                 arr = (char **)realloc(arr, sizeof(char *)*numWords);
                                 arr[idx] = (char *)malloc(sizeof(char)*30);
-                                strcpy(arr[idx], words[i]);
+                                //add it to array
+                                strcpy(arr[idx], words[i]); 
+                                //update index
                                 idx++;
+                                //break the loop
                                 break;
                             }
                         }
