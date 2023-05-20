9901347d9259b7830a913b368dfb3ae550372eb7
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-05-20 12:46:53 -0600

    Time: 74 ms (6.32%), Space: 6.9 MB (44.21%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.c
@@ -0,0 +1,53 @@
+void deleteAndAppend(int *arr, int numsSize, int idx)
+{
+    int val = arr[idx]; 
+    for(int i = idx; i<numsSize-1; i++)
+    {
+        arr[i] = arr[i+1]; 
+    }
+    arr[numsSize-1] = val; 
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* applyOperations(int* nums, int numsSize, int* returnSize){
+
+    *returnSize = numsSize; 
+    
+    int *arr = (int *)malloc(sizeof(int)*numsSize);
+    memcpy(arr, nums, (sizeof(int)*numsSize)); 
+    
+    int zeroes = 0; 
+    for(int i=0; i<numsSize-1; i++)
+    {
+        if(arr[i] == 0)
+        {
+            zeroes++; 
+        }
+        if(arr[i] == arr[i+1])
+        {
+            arr[i] *= 2; 
+            arr[i+1] = 0;
+        }
+    }
+    
+    printf("zeroes = %d\n", zeroes); 
+    
+    for(int i=0; i<numsSize; i++)
+    {
+        if (arr[i] == 0)
+        {
+            deleteAndAppend(arr, numsSize, i); 
+            zeroes--; 
+            if ((i != 0) && (zeroes > 0))
+            {
+                i--;
+            }
+                
+  
+        }
+    }
+    
+    return arr; 
+}
