e8098e4653687226d9fef057f48407e0e1c62fd4
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-07-09 21:05:28 -0700

    Time: 20 ms (8.41%), Space: 10.6 MB (58.66%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        
+        int rows = matrix.size(); 
+        int cols = matrix[0].size();
+        
+        vector<vector<int>> retmtx (cols); 
+        
+        int ri = 0, ci = 0, rri = 0;
+        
+        for(ri = 0; ri < rows; ri++)
+        {
+            for(ci = 0; ci < cols; ci++)
+            {
+                retmtx[rri].push_back(matrix[ri][ci]);
+                rri++;
+            }
+            rri = 0; 
+        }
+        
+        return retmtx; 
+    }
+};
