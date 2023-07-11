7188ec95ead164a9b3b561bef71c5facc23605d6
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-07-10 23:28:37 -0700

    Time: 3 ms (54.55%), Space: 5.4 MB (90.91%) - LeetHub

---
+++ 2525-categorize-box-according-to-criteria/2525-categorize-box-according-to-criteria.c
@@ -0,0 +1,49 @@
+enum {
+    heavy, 
+    bulky, 
+    neither, 
+    both
+};
+
+#define ONE_HUNDRED     (100)
+#define TEN_THOUSAND    (10000)
+#define ONE_BILLION     (1000000000)
+
+char * categorizeBox(int length, int width, int height, int mass){
+
+    char *arr[4] = {"Heavy", "Bulky", "Neither", "Both"}; 
+    bool isBulky = false, isHeavy = false; 
+    
+    if (mass >= ONE_HUNDRED)
+    {
+        isHeavy = true; 
+    }
+    if (length >= TEN_THOUSAND || width >= TEN_THOUSAND || height >= TEN_THOUSAND)
+    {
+        isBulky = true; 
+    }
+
+    if(isBulky == false)
+    {
+        long long volume=(long long)(length)*(long long)(width)*(long long)height;
+        if (volume >= ONE_BILLION)
+        {
+            isBulky = true; 
+        }
+    }
+
+    
+    if (isBulky == false && isHeavy == false)    
+    {
+        return arr[neither]; 
+    }
+    else if (isBulky == false && isHeavy == true)    
+    {
+        return arr[heavy]; 
+    }
+    else if (isBulky == true && isHeavy == false)    
+    {
+        return arr[bulky]; 
+    }
+    return arr[both];    
+}
