93ee606674343d98d9d6647d5da40566ca57cfac
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-07-15 15:27:43 -0700

    Time: 3 ms (55.21%), Space: 5.5 MB (67.19%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.c
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.c
@@ -1,29 +1,11 @@
-
-
 int xorOperation(int n, int start){
     
-    /*define array of n integers*/
-    int *arr = (int *)malloc(sizeof(int)*n);
-
-    int retval = 0;
+    int xor = 0;
     
-    /*populate the array*/
-    for(int i = 0; i<n; i++)
+    for(int i=0; i<n; i++)
     {
-        arr[i] = start+(2*i);
-    }
+        xor ^= (start + (2*i));
+    }    
     
-    /*get first element*/
-    retval = arr[0];
-    
-    /*perform xor*/
-    for(int i = 1; i<n; i++)
-    {
-        retval ^= arr[i];
-    }
-    
-    free(arr);
-    
-    return retval;
-
+    return xor; 
 }
