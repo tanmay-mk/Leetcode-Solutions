c877ad78ebc2ea7728757cd725bebab56557f71f
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-07-23 17:50:59 -0700

    Time: 0 ms (100.00%), Space: 6 MB (5.26%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.c
+++ 1046-last-stone-weight/1046-last-stone-weight.c
@@ -7,9 +7,7 @@ int lastStoneWeight(int* stones, int stonesSize){
 
     while(stonesSize > 1)
     {
-        
         qsort(stones, stonesSize, sizeof(int), compare);
-        
         stones[stonesSize-2] = stones[stonesSize-1] - stones[stonesSize-2];
         stonesSize--; 
     }
