ae424d25bc51bada616e1e4ca0e660abeca65b29
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-08-13 23:13:35 -0700

    Time: 11 ms (92.24%), Space: 6.3 MB (78.45%) - LeetHub

---
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.c
@@ -0,0 +1,14 @@
+int maximumCount(int* nums, int numsSize){
+
+    int pos = 0, neg = 0; 
+    
+    for(int i=0; i<numsSize; i++)
+    {
+        if (nums[i] != 0)
+        {
+            (nums[i] > 0)? pos++ : neg++; 
+        }
+    }
+    
+    return ((pos > neg)? pos : neg); 
+}
