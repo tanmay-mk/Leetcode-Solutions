b57f259584be1710719c58e94a9d32d9b63684d1
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-09-05 21:47:46 -0700

    Time: 7 ms (29.17%), Space: 5.9 MB (91.67%) - LeetHub

---
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.c
@@ -0,0 +1,28 @@
+#define CONCAT(a,b) (a##b)
+
+long long findTheArrayConcVal(int* nums, int numsSize){
+
+    if(numsSize == 1)
+    {
+        return ((long long) nums[0]);
+    }
+    
+    long long answer = 0; 
+    char arr[15] = {0}; 
+    int i = 0, j = (numsSize - 1); 
+    
+    while(i < j)
+    {
+        memset(arr, 0, sizeof(char)*15);
+        sprintf(arr, "%d%d", nums[i], nums[j]);
+        answer += atoi(arr);
+        
+        i++; j--;
+        
+        if(i == j)
+        {
+            answer += nums[i]; 
+        }
+    }
+    return answer; 
+}
