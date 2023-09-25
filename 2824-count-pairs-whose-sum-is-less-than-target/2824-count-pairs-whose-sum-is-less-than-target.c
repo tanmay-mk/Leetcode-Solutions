d438bf5bb95080e193b247a44648cb9c09a17595
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-09-24 18:58:41 -0700

    Time: 0 ms (100.00%), Space: 6.7 MB (14.07%) - LeetHub

---
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.c
@@ -0,0 +1,15 @@
+int countPairs(int* nums, int numsSize, int target){
+
+    int count=0;
+    for(int i=0; i<numsSize-1; i++)
+    {
+        for (int j = i+1; j<numsSize; j++)
+        {
+            if(nums[i]+nums[j] < target)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
