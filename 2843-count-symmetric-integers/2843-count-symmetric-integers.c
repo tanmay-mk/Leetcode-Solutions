d01cd27fab34a25028b86b8d3f8ee9a51656b001
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2023-09-24 19:35:27 -0700

    Time: 20 ms (37.00%), Space: 6.3 MB (5.50%) - LeetHub

---
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.c
@@ -0,0 +1,48 @@
+void my_itoa_decimal(int n, char *buff)
+{
+    int idx = 0; 
+    while(n > 0)
+    {
+        int rem = n%10; 
+        buff[idx++] = rem;
+        n/=10; 
+    }
+    for(int i=0; i<idx/2; i++)
+    {
+        buff[i] ^= buff[idx-i-1];
+        buff[idx-i-1] ^= buff[i];
+        buff[i] ^= buff[idx-i-1];
+    }
+}
+
+
+int countSymmetricIntegers(int low, int high){
+
+    int count = 0;
+    char number[5] = {0};
+    
+    while ((low <= high) && (low < 10000))
+    {
+        if ((low >= 10) && (low < 100))
+        {
+            //2 digit number, check if both digits are same
+            my_itoa_decimal(low, number); 
+            if(number[0] == number[1])
+            {
+                count++;
+            }
+            memset(number, 0, 5); 
+        }
+        else if ((low >= 1000) && (low < 10000))
+        {
+            my_itoa_decimal(low, number); 
+            if (((number[0] - '0')+(number[1] - '0')) == ((number[2] - '0')+(number[3] - '0')))
+            {
+                count++;
+            }
+            memset(number, 0, 5);
+        }
+        low++;  
+    }
+    return count; 
+}
